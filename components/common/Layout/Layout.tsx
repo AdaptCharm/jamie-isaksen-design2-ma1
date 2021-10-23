912f32db383d2f520d05e13dc4b779793976dd04
AdaptCharm <jamie@jamie.no>
2021-10-23 17:43:38 +0200

    Add footer component

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react'
-import { Navbar } from '@components/common'
+import { Navbar, Footer } from '@components/common'
 
 const Layout: FC = ({ children }) => {
   return (
     <>
       <Navbar />
       <main>{children}</main>
+      <Footer />
     </>
   )
 }
